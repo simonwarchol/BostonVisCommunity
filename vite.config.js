cca24ec27ef69f7a86f6562e7dd8c7bc3756d337
Simon Warchol <simonwarchol@g.harvard.edu>
2024-08-23 14:51:30 -0400

    Removing url

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/<REPO_NAME>/', // Replace <REPO_NAME> with your repository name
   plugins: [react()],
 })
