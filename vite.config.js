b685bba191c5fd8bab5c52782c1b681927a0619d
Simon Warchol <simonwarchol@g.harvard.edu>
2024-08-23 14:31:48 -0400

    Adding vite base

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/<REPO_NAME>/', // Replace <REPO_NAME> with your repository name
   plugins: [react()],
 })
