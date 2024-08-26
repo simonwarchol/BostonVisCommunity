5737495c72c3c066bd7292a094aed4c8d32218ff
cbrumar <cbrumar@alifehealth.com>
2024-08-26 14:51:27 -0400

    updated the calendar link

--- src/components/Calendar/Calendar.tsx
+++ src/components/Calendar/Calendar.tsx
@@ -8,7 +8,7 @@ const Calendar = () => {
 
             <iframe
                 id='calendar-iframe'
-                src="https://calendar.google.com/calendar/u/0?cid=NTVmNWRmMzRkNGVkODU1NWU3YTliZmNhNWE2NmIwMjNjZjMzNjAxY2NlM2ZlOTA0NmQxYzkzNTc5NWMwYmI2ZkBncm91cC5jYWxlbmRhci5nb29nbGUuY29t"
+                src="https://calendar.google.com/calendar/embed?src=55f5df34d4ed8555e7a9bfca5a66b023cf33601cce3fe9046d1c935795c0bb6f%40group.calendar.google.com&ctz=America%2FNew_York"
             >
             </iframe>
         </section>
