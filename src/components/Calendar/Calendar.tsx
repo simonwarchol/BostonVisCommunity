58b165beddc32d32736fa1273e0c0e170757d507
cbrumar <cbrumar@alifehealth.com>
2024-08-26 14:42:40 -0400

    changed the calendar link

--- src/components/Calendar/Calendar.tsx
+++ src/components/Calendar/Calendar.tsx
@@ -6,7 +6,7 @@ const Calendar = () => {
             <h2 className='section__title'>Calendar</h2>
 
             <iframe
-                src="https://calendar.google.com/calendar/embed?height=600&wkst=1&ctz=America%2FNew_York&bgcolor=%23ffffff&src=NTVmNWRmMzRkNGVkODU1NWU3YTliZmNhNWE2NmIwMjNjZjMzNjAxY2NlM2ZlOTA0NmQxYzkzNTc5NWMwYmI2ZkBncm91cC5jYWxlbmRhci5nb29nbGUuY29t&color=%23AD1457"
+                src="https://calendar.google.com/calendar/u/0?cid=NTVmNWRmMzRkNGVkODU1NWU3YTliZmNhNWE2NmIwMjNjZjMzNjAxY2NlM2ZlOTA0NmQxYzkzNTc5NWMwYmI2ZkBncm91cC5jYWxlbmRhci5nb29nbGUuY29t"
                 style={{ border: 'solid 1px #777', margin: '1em' }}
                 width="800"
                 height="600"
