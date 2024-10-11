e91239a2e162c2943d03a488d583ae5d580fe6de
Simon Warchol <simonwarchol@g.harvard.edu>
2024-10-11 10:21:37 -0400

    Adding pdf

--- src/components/Vis/Vis.jsx
+++ src/components/Vis/Vis.jsx
@@ -5,22 +5,25 @@ import './../../App.css';
 
 const Vis = () => {
   // Use the embeddable Google Drive URL for previewing the PDF
-  const pdfUrl = 'https://drive.google.com/file/d/1O65slksC7PVVev67Dsvok0T2y0de8E1Y/preview';
+  // const pdfUrl = 'https://drive.google.com/file/d/1O65slksC7PVVev67Dsvok0T2y0de8E1Y/preview';
+  const pdfUrl = 'https://drive.google.com/file/d/1ExPFegSKCRYduU2u4Ig6WhADbexHOZQC/preview';
 
   return (
     <div className="vis-container">
       {/* Banner above the PDF viewer */}
       <div className="banner">
         <div className="banner-content">
-          <span className="banner-date">(10.10.2024)</span>
-          <span className="banner-text">IEEE VIS "Boston" Agenda</span>
+          <span className="banner-date">Oct 13-18, 2024</span>
+          <span className="banner-text">IEEE Mini-Vis Boston</span>
+        </div>
+        <div className="rsvp-content">
+          {/* RSVP :https://join.slack.com/t/boston-vis/shared_invite/zt-2s4vkbvg5-OCFX~flb61l_6YevGkTNNA */}
+          <span><a href="https://forms.gle/BGPeSUtmgoHgha2c9" target="_blank" rel="noreferrer">RSVP</a></span>
+          <span><a href="https://join.slack.com/t/boston-vis/shared_invite/zt-2s4vkbvg5-OCFX~flb61l_6YevGkTNNA" target="_blank" rel="noreferrer">Slack</a></span>
+          <span><a href={pdfUrl} target="_blank" rel="noreferrer">Agenda</a></span>
         </div>
       </div>
-      <div className="rsvp-content">
-        {/* RSVP :https://join.slack.com/t/boston-vis/shared_invite/zt-2s4vkbvg5-OCFX~flb61l_6YevGkTNNA */}
-        <span><a href="https://forms.gle/BGPeSUtmgoHgha2c9" target="_blank" rel="noreferrer">RSVP</a></span>
-        <span><a href="https://join.slack.com/t/boston-vis/shared_invite/zt-2s4vkbvg5-OCFX~flb61l_6YevGkTNNA" target="_blank" rel="noreferrer">SLACK</a></span>
-      </div>
+
 
       {/* Embed PDF */}
       <div className="pdf-viewer">
