8c5bc8f81a4ce3c66ed596a11fbf789bdbe09d9c
Simon Warchol <simonwarchol@g.harvard.edu>
2024-10-11 10:49:58 -0400

    adding new stuff

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import { useContext } from 'react'
 import About from './components/About/About'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
 import Events from './components/Events/Events'
 import ScrollToTop from './components/ScrollToTop/ScrollToTop'
 import Contact from './components/Contact/Contact'
@@ -12,27 +14,40 @@ import Participants from './components/Participants/Participants'
 import Vis from './components/Vis/Vis'
 import Description from './components/Description/Description'
 
+const Redirect = ({url}) => {
+  window.location.href = url;
+  return null;
+};
+
 const App = () => {
 
   return (
     <>
-      <Navbar />
-
-      <main>
-        <About />
-        <Vis />
-        <
-          Description/>
-        <Events />
-        <Calendar />
-        <Participants />
-        <Contact />
-      </main>
-      <Background />
-      <ScrollToTop />
-      <Footer />
+      <Router>
+        <Routes>
+          <Route path="/" element={(
+            <>
+              <Navbar />
+              <main>
+                <About />
+                <Vis />
+                <Description />
+                <Events />
+                <Calendar />
+                <Participants />
+                <Contact />
+              </main>
+              <Background />
+              <ScrollToTop />
+              <Footer />
+            </>
+          )} />
+          <Route path='/slack' element={<Redirect url={'https://join.slack.com/t/boston-vis/shared_invite/zt-2s4vkbvg5-OCFX~flb61l_6YevGkTNNA'}  />} />
+          <Route path='/rsvp' element={<Redirect url={'https://forms.gle/BGPeSUtmgoHgha2c9'}  />} />
+        </Routes>
+      </Router>
     </>
   )
 }
 
-export default App
+export default App;
