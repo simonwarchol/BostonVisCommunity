3d688c75137169cfa9e5a6e92ffe21ece815a5a5
Simon Warchol <simonwarchol@g.harvard.edu>
2024-10-14 18:28:20 -0400

    New website

--- src/components/Vis/Vis.jsx
+++ src/components/Vis/Vis.jsx
@@ -4,8 +4,6 @@ import './Vis.css';
 import './../../App.css';
 
 const Vis = () => {
-  // Use the embeddable Google Drive URL for previewing the PDF
-  // const pdfUrl = 'https://drive.google.com/file/d/1O65slksC7PVVev67Dsvok0T2y0de8E1Y/preview';
   const pdfUrl = 'https://drive.google.com/file/d/1ExPFegSKCRYduU2u4Ig6WhADbexHOZQC/preview';
 
   return (
@@ -13,16 +11,24 @@ const Vis = () => {
       {/* Banner above the PDF viewer */}
       <div className="banner">
         <div className="banner-content">
-          <span className="banner-date vis__section__title">Oct 13-18, 2024</span>
-          <span className="banner-text vis__section__title">Mini-Vis Boston</span>
-        </div>
-        <div className="rsvp-content">
-          {/* RSVP :https://join.slack.com/t/boston-vis/shared_invite/zt-2s4vkbvg5-OCFX~flb61l_6YevGkTNNA */}
-          <span className="vis__section__subtitle"><a href="https://forms.gle/BGPeSUtmgoHgha2c9" target="_blank" rel="noreferrer">RSVP</a></span>
-          <span className="vis__section__subtitle"><a href="https://join.slack.com/t/boston-vis/shared_invite/zt-2s4vkbvg5-OCFX~flb61l_6YevGkTNNA" target="_blank" rel="noreferrer">Slack</a></span>
+          <div className="banner-logo-parent">
+            <img src={'/BosVisLogo.png'} alt="Vis Logo" className="banner-logo" />
+          </div>
+          <div className="banner-text-content">
+            <div className='text-child'>
+              <span className="banner-date vis__section__title">Oct 13-18, 2024</span>
+            </div>
+            <div className='text-child'>
+
+              <span className="banner-text vis__section__title">Mini-Vis Boston</span>
+            </div>
+            <div className='text-child'>
+              <span className="vis__section__subtitle rsvp-text"><a href="https://forms.gle/BGPeSUtmgoHgha2c9" target="_blank" rel="noreferrer">RSVP</a></span>
+              <span className="vis__section__subtitle rsvp-text"><a href="https://join.slack.com/t/boston-vis/shared_invite/zt-2s4vkbvg5-OCFX~flb61l_6YevGkTNNA" target="_blank" rel="noreferrer">Slack</a></span>
+            </div>
+          </div>
         </div>
       </div>
-
 
       {/* Embed PDF */}
       <div className="pdf-viewer">
@@ -32,7 +38,6 @@ const Vis = () => {
           title="PDF Viewer"
         ></iframe>
       </div>
-
     </div>
   );
 };
